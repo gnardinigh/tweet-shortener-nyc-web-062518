e61dd249dbdbf24df8fd5ae9d251aedebce996b5
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:44:18 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,7 +40,6 @@ def bulk_tweet_shortener(array_of_tweets)
   array_of_tweets.each_with_index do |tweet,index|
     this_tweet = array_of_tweets[index]
     array_of_tweets[index] = word_substituter(this_tweet)
-    puts array_of_tweets[index]
   end
   
 end
