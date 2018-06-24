23c19272faac5cde0b7b234eef5c4e449b953f52
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:03:23 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,8 +4,17 @@
 
 def dictionary
   dictionary_hash={
-              "hello" => 
-  }
+              "hello" => "hi",
+              "to" => "2"
+              "two" => "2"
+              "too" => "2"
+              "four" => "4"
+              "for" => "4"
+              "be" => "b"
+              "you" => "u"
+              "at" => "@"
+              "and" => "&"
+  } 
   
 end
 
