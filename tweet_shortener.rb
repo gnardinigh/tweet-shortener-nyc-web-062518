cea607628814648a3a0df95b91409c884235315f
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:12:31 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,7 +15,7 @@ def dictionary(tweet)
               "at" => "@",
               "and" => "&",
   }
-  
+  tweet_array = tweet.split(" ")
 end
 
 tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
