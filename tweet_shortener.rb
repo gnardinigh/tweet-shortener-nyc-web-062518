1d78b806f57772a9d89095de833ad5ae44ad64eb
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 18:22:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -59,7 +59,7 @@ tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the b
 def shortened_tweet_truncator(tweet)
   shortened_tweet = word_substituter(tweet)
   if shortened_tweet.size>140
-    puts shortened_tweet.size
+    shortened_tweet = shortened_tweet.slice[0..139]
   end
 end
 
