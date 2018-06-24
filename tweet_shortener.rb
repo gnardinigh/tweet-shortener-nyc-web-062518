3bf165d84cfcb038ca4adaf704f42b7945a9090c
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:06:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -22,7 +22,10 @@ tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the b
 def word_subsituter(tweet)
   tweet_array = tweet.split(" ")
   tweet_array.each do |word|
-    
+    dictionary_hash.each do |key,value|
+      
+    end
+    counter+=1
   end 
 end
 
