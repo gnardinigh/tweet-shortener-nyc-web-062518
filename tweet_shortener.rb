87adb8037278aa15ddf54ca12c04e2dbe4253cea
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:30:36 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,7 +23,7 @@ def dictionary(tweet)
   tweet_array.each_with_index do |word|
       dictionary_keys.each do |key|
         if word == key
-          puts "yay" + key
+          puts "yay" + dictionary_keys[key]
       end
     end
   end
