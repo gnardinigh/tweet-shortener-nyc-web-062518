4cbbc70ce7009383d2cc4a36ac4ab140d4e1d311
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:29:35 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,7 +23,7 @@ def dictionary(tweet)
   tweet_array.each_with_index do |word,index|
       dictionary_keys.each do |key|
         if word == key
-          tweet_array[index]=dictionary_keys[key]
+          
       end
     end
   end
