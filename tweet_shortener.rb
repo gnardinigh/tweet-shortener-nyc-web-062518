034e1b9fbd20140a2a8788c7c386a1d423324b6c
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:04:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,7 +19,9 @@ end
 
 tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
-
+def word_subsituter(tweet)
+  
+end
 
 
 
