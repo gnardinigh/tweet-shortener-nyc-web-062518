a67a8bbd696b6821d10af1f99a62f74aaf92b0f0
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:03:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,7 +17,7 @@ def dictionary
   } 
 end
 
-
+tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
 
 
