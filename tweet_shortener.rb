e2c426732a76f965dfd509dd2e3b20191eea1a53
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:12:13 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@
 
 
 
-def dictionary
+def dictionary(tweet)
   dictionary_hash={
               "hello" => "hi",
               "to" => "2",
@@ -14,26 +14,13 @@ def dictionary
               "you" => "u",
               "at" => "@",
               "and" => "&",
-  } 
+  }
+  
 end
 
 tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
-def word_subsituter(tweet,dictionary)
-  tweet_array = tweet.split(" ")
-  counter=0
-  tweet_array.each do |word|
-    dictionary_hash.each do |key,value|
-      if word==key
-        tweet_array[counter] = value
-     end
-    end
-    counter+=1
-  end 
-  puts tweet_array.inspect
-end
 
-word_subsituter(tweet)
 
 
 
