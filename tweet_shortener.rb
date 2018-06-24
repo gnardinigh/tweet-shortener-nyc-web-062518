373edced7505269943997e6a6487fde2deb56eb5
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-24 17:10:25 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,15 +5,15 @@
 def dictionary
   dictionary_hash={
               "hello" => "hi",
-              "to" => "2"
-              "two" => "2"
-              "too" => "2"
-              "four" => "4"
-              "for" => "4"
-              "be" => "b"
-              "you" => "u"
-              "at" => "@"
-              "and" => "&"
+              "to" => "2",
+              "two" => "2",
+              "too" => "2",
+              "four" => "4",
+              "for" => "4",
+              "be" => "b",
+              "you" => "u",
+              "at" => "@",
+              "and" => "&",
   } 
 end
 
